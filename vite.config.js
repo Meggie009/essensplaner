43a92cf916f940dc8945c6d33e5233d678a417ad
Magdalena Allmann <allmann@rhrk.uni-kl.de>
2026-09-05 18:27:59 +0200

    changed name

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
-// IMPORTANT: replace 'meal-planner' with your actual GitHub repo name.
+// IMPORTANT: replace 'essensplaner' with your actual GitHub repo name.
 // This must match https://<username>.github.io/<repo-name>/
 export default defineConfig({
   plugins: [svelte()],
-  base: '/meal-planner/',
+  base: '/essensplaner/',
 });
